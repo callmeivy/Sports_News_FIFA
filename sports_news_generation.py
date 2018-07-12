11215c9662d79e63b694ad1254eeedf0e87afeba
ivy <c.ivy.jin@foxmail.com>
2018-07-12 11:18:10 +0800

    完善了日期时间获取，增加队伍获取

--- sports_news_generation.py
+++ sports_news_generation.py
@@ -12,6 +12,7 @@ from nltk.corpus import treebank
 data = dict()
 path = '/home/ivy/文档/wanxiaojun/NLPCC2016Eval-Task5-AllData/sampleData/SampleData'
 
+# 读取比赛名称库赌
 def match_name():
     match_names = list()
     with open('match_name', 'r') as f:
@@ -21,6 +22,12 @@ def match_name():
     f.close()
     return match_names
 
+# 读取比赛方式
+def match_type_chinese():
+    ma = ["决赛", "半决赛", "四分之一决赛", "八分之一决赛", "预赛", "淘汰赛"]
+    return ma
+
+# 获取比赛名称及时间
 def get_time_match_name():
     match_names = match_name()
     name_numer = list()
@@ -36,15 +43,28 @@ def get_time_match_name():
                     for i in range(10):  # 比赛名称、日期、时间一般出现在文字直播前十行，不需要全文读取
                         line = f.readline().strip()
                         # print "line", line
+                        number = (re.findall('(\d+)', line[:len(line) - 4]))  # 数字抽取
                         # 开始查找比赛名称
-                        if not name_found_tag:  # 一旦找到，就不用继续下文了
-                            for name in match_names:
+                        for name in match_names:
+                            if not name_found_tag:  # 一旦找到，就不用继续下文了
                                 if name in line:
-                                    number = (re.findall('(\d+)', line[:len(line)-4]))  # 数字抽取，意在抓取第几轮比赛
-                                    print type(number)
                                     if len(number) == 0:
-                                        number = 0
-                                    print "111", name, number, dir_item, line
+                                        match_number = 0
+                                    if len(number) > 1:
+                                        if line.index(name) < line.index(number[-1]):  # 比赛名称中的数字定在名称之后，防止提取出来是日期数字等
+                                            name_match = name
+                                            match_number = number
+                                            if number[0] + '/' + number[1] in line:
+                                                match_number = number[0] + '/' + number[1] + "决赛"  # 1/8决赛
+                                            if number[0] + '-' + number[1] in line:
+                                                match_number = number[0] + '-' + number[1]  # 15-16赛季英超联赛
+                                    else:
+                                        name_match = name
+                                        match_number = number
+                                    for ma_ty in match_type_chinese():  # 八分之一决赛
+                                        if ma_ty in line:
+                                            name_match = name_match + ma_ty
+                                    # print "111", name_match, match_number, dir_item, line
                                     name_found_tag = True
                                     # break  # name_found_tag判断就可以，无需break
 
@@ -52,35 +72,46 @@ def get_time_match_name():
                         # 注意：日期与时间必须同时提。否则，日期提取完成后，有可能时间与日期在同一行，但日期已经break，
                         # 那么可能导致时间获取不到
                         # ===============================================================================
-                        else:
-                            number = (re.findall('(\d+)', line[:len(line) - 4]))
-                            if len(number) >= 2:
-                                for index in range(len(number)-1):  # 注意“-1”，否则“list index out of range”
-                                    # 开始查找比赛日期
-                                    if not date_found_tag:
-                                        if number[index] + "月" + number[index+1] + "日" in line:  # 日期匹配 匹配“2月3日”
-                                            date = number[index] + "月" + number[index+1] + "日"
-                                            date_found_tag = True
-                                    # 开始查找比赛时间
-                                    if not time_found_tag:
-                                        if number[index] + "：" + number[index+1] in line:  # 时间匹配，注意中文":"，匹配“3：45”
-                                            if len(str(number[index])) + len(str(number[index+1])) > 2:  # 避免混淆比分，如1：5
-                                                print "index2", index
-                                                time = number[index] + "：" + number[index+1]
-                                                print "333", time, line, dir_item
-                                                time_found_tag = True
-                                                continue
-                                        if number[index] + "点" + number[index+1] in line:  # 匹配“3点45”
-                                            time = number[index] + "点" + number[index+1]
-                                            print "334", time, line, dir_item
+                        if type(number) == list and len(number) >= 2:
+                            for index in range(len(number)-1):  # 注意“-1”，否则“list index out of range”
+                                # 开始查找比赛日期
+                                if not date_found_tag:
+                                    if number[index] + "月" + number[index+1] + "日" in line:  # 日期匹配 匹配“2月3日”
+                                        date = number[index] + "月" + number[index+1] + "日"
+                                        date_found_tag = True
+                                # 开始查找比赛时间
+                                if not time_found_tag:
+                                    if number[index] + "：" + number[index+1] in line:  # 时间匹配，注意中文":"，匹配“3：45”
+                                        if len(str(number[index])) + len(str(number[index+1])) > 2:  # 避免混淆比分，如1：5
+                                            time = number[index] + "：" + number[index+1]
+                                            # print "333", time, line, dir_item
                                             time_found_tag = True
+                                            continue
+                                    if number[index] + "点" + number[index+1] in line:  # 匹配“3点45”
+                                        time = number[index] + "点" + number[index+1]
+                                        # print "334", time, line, dir_item
+                                        time_found_tag = True
                 if not date_found_tag:
                     date = "#"
                 if not time_found_tag:
                     time = "#"
-                print "name, number,date,time,dir_item",name, dir_item,number,dir_item,date,dir_item,time,dir_item
+                print "name, number,date,time,dir_item",name_match,match_number,date,time,dir_item
     f.close()
 
+# 获取比赛队名
+def get_match_teams():
+    for dir_item in os.listdir(path):
+        if dir_item.endswith("tec.csv"):
+            dir_item_path = os.path.join(path, dir_item)
+            if os.path.isfile(dir_item_path):
+                with open(dir_item_path, 'r') as f:
+                    for i in range(1):
+                        line = f.readline().strip()
+                        teams = line.split(",")[0] + "," + line.split(",")[2]
+                        print teams, dir_item
+
+
 if __name__ == '__main__':
-    get_time_match_name()
+    # get_time_match_name()
     # match_name()
+    get_match_teams()
