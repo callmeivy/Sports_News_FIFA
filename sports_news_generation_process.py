99da533a6ba6ab97b74ba9817a7d05cbec2a9644
ivy <c.ivy.jin@foxmail.com>
2018-07-19 14:52:16 +0800

    none

--- sports_news_generation_process.py
+++ sports_news_generation_process.py
@@ -24,28 +24,45 @@ def get_process():
             before_after_time = dict()
             scripts_time = dict()
             dir_item_path = os.path.join(path, dir_item)
+            time_box = list()
+            key_box = list()
             if os.path.isfile(dir_item_path):
                 with open(dir_item_path, 'r') as f:
                     lines = f.readlines()
+                    ind = 0
                     for line in lines:
                         line = line.strip().split(",")
                         # print "lineline", line[1]
-                        if line[1] == "未赛":
+                        # if line[1] == "未赛" or line[1] == "中场" or line[1] == "完赛":
+                        if "上半场" not in line[1]:
                             continue
-                        time = line[1].split(" ")[1].replace("'", "")
+                        time = line[1].split(" ")[0][:3] + line[1].split(" ")[1].replace("'", "")
+                        # print "time", time, time[3:]
+                        # 这里以文字内容为key,但其实文字内容也有重复的，以10.live为例，实际上半场直播有133条，但key值只有131条，但本系统
+                        # 认为影响不大，重复的key值默认被删除
                         scripts_time[line[0]] = time
-                        # scripts_time.append(line[0] + "   " + time)
-                        for word in exciting_moment:
-                            for k, v in scripts_time.items():
-                                if word in k:
-                                    print k, v
-                                    print k
-                                    before_after.append(k)
-                                    for key, value in scripts_time.items():
-                                        if value == v-1 or value == v+1:  # 如果判断是精彩瞬间，就把此前1分钟和此后1分钟的文本结合
-                                            before_after.append(key)
-                                    before_after = before_after.join("。")
-                                    before_after_time[v-1] = before_after
+                    for word in exciting_moment:
+                        print "word1", word
+                        for k, v in scripts_time.items():
+                            before_after = list()
+                            ind = 0
+                            if word in k:
+                                print "word2", word
+                                ind += 1
+                                if ind > 1:
+                                    break
+                                print k, v
+                                print k
+                                before_after.append(k)
+                                for key, value in scripts_time.iteritems():
+                                    print 22222, int(v[3:])-1, value, int(v[3:])+1
+                                    if value == v[:3] + str(int(v[3:])-1) or value == v[:3] + str(int(v[3:])+1):  # 如果判断是精彩瞬间，就把此前1分钟和此后1
+                                        # 分钟的文本结合
+                                        print "jjj", key, value
+                                        before_after.append(key)
+                                before_after_str = "。".join(before_after)
+                                print "before_after_str", before_after_str, v[:3] + str(int(v[3:])-1)
+                                before_after_time[v[:3] + str(int(v[3:])-1)] = before_after_str
 
 
     f.close()
