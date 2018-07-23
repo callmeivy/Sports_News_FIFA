2ea7eeab2c2e4e4dee69b446993cad00fff0f2aa
ivy <c.ivy.jin@foxmail.com>
2018-07-23 17:20:29 +0800

    none

--- try.py
+++ try.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# file: sports_news_generation_p1.py
-# author: Ivy Jin(c.ivy.jin@foxmail.com)
-# time: 07/23/2018 09:40 AM
-# 体育新闻第一段生成，基于FIFA数据及CNTV文字直播内容
-# 第一段模板来自C5"全场战报"
-# Copyright 2018 Ivy Jin. All Rights Reserved.
+# line = 'abcdef'
+# # a.replace('a','v').replace('b','g')
+# line = line.replace('a','v').replace('b','g')
+# print line
+# a = ['1']
+# a.append("2")
+# print a[::-1]
+# a = [1,2]
+# a.append([4,5])
+# print a
 import os
 import json
 import datetime
@@ -14,32 +18,33 @@ from datetime import timedelta
 path = '/home/ivy/PycharmProjects/Sports_News_FIFA/20180715'
 
 
-# 获取自定义翻译词典
+# 获取基本中英翻译
 def get_en_ch():
     en_to_ch = list()
     with open('en_to_ch', 'r') as f:
         for line in f.readlines():
+            print "line", line
             line = line.strip().split("   ")
             en_to_ch.append(line)
     f.close()
     return en_to_ch
 
 
-# 基本英文翻译成中文
 def traslation(input_word):
     output_word = ""
     en_to_ch = get_en_ch()
     input_word_lo = input_word.lower()  # 基本翻译 为小写, 注意()
     for line in en_to_ch:
         # 如果找不到，单词转为全部小写
+        print line[0], line[1]
         if input_word_lo == line[0] or input_word == line[0]:
             output_word = line[1]
             break
     return output_word
 
 
-#  第一段：获取球赛基本情况，并根据相应的句子模板，生成句子
-def generate_para1():
+
+def loadinfo():
     for dir_item in os.listdir(path):
         if dir_item.endswith("getMatchInfo.json"):
             dir_item_path = os.path.join(path, dir_item)
@@ -64,16 +69,15 @@ def generate_para1():
                     timearray = timearray + timedelta(hours = 8 )
                     #  第几轮比赛转为中文
                     round_name = traslation(roundname)
+                    print "b4", stadiumname
                     stadiumname = traslation(stadiumname)
+                    print stadiumname
                     venuename = traslation(venuename)
                     hometeam = traslation(hometeam)
                     awayteam = traslation(awayteam)
-                    para_1 = '北京时间{0}年{1}月{2}日{3}点，俄罗斯世界杯{4}在{5}{6}打响，{7}迎战{8}。'.format(
-                        timearray.year, timearray.month, timearray.day, timearray.hour, round_name, venuename,
-                        stadiumname, hometeam, awayteam)
-    print para_1
-    return para_1
+                    print '北京时间{0}年{1}月{2}日{3}点，俄罗斯世界杯{4}在{5}{6}打响，{7}迎战{8}。'.format(timearray.year,
+                        timearray.month, timearray.day, timearray.hour, round_name, venuename, stadiumname, hometeam, awayteam)
 
 
 if __name__ == '__main__':
-    generate_para1()
+    loadinfo()
