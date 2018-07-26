5c91b9056b0a7ebac0c6800f4cb351807fb18a3d
callmeivy <c.ivy.jin@foxmail.com>
2018-07-26 17:54:07 +0800

    none

--- sports_news_generation_FIFA.py
+++ sports_news_generation_FIFA.py
@@ -10,6 +10,7 @@ import os
 import json
 import datetime
 from datetime import timedelta
+from collections import Counter
 
 # path = '/home/ivy/PycharmProjects/Sports_News_FIFA/20180715'
 path = 'F:\工作\Fifa_2018\\20180715\\random'
@@ -107,6 +108,7 @@ def generate_first_round_lineup():
 #  换人，不仅是换人，所有的event都提取出来了
 def generate_change():
     event_box = list()
+    total_type = list()
     path_0 = r"F:\工作\Fifa_2018"  # 换人一般在60分钟以后，样本数据囊括不到
     days = ['20180715', '20180716']
     for day in days:
@@ -116,19 +118,12 @@ def generate_change():
                 if (dir_item.startswith("2018071523")) or (dir_item.startswith("2018071600")):  # 决赛跨天，确保是决赛记录
                     dir_item_path = os.path.join(path_1, dir_item)  # isfile后需是路径，而不仅是文件名，所以需要join
                     if os.path.isfile(dir_item_path):  # isfile后需是路径，而不仅是文件名
-                        print('hou', dir_item)
                         with open(dir_item_path, 'r') as f:
                             match_info = json.load(f)
                             # print(match_info["MatchEvents"])
                             for one in match_info["MatchEvents"]:
                                 #  事件类型
                                 event_type = one["Type"]
-                                # Goal: 找出进球队及队员
-                                if event_type == 'Goal':
-                                    print(one)
-                                    playerfromid = one["PlayerFromId"]
-                                    teamtoid = one["TeamToId"]
-                                    print(playerfromid, teamtoid)
                                 # print(event_type)
                                 #  事件发生分钟数
                                 event_min = one["Minute"]
@@ -138,30 +133,66 @@ def generate_change():
                                 type_event.append(event_type)
                                 type_event.append(event_min)
                                 if type_event not in event_box:
-                                    print(type_event)
+                                    # print(type_event)
+                                    total_type.append(event_type)
                                     event_box.append(type_event)
+                                    # Goal: 找出进球队及队员
+                                    if event_type == 'Goal':
+                                        playerfromid_goal = one["PlayerFromId"]
+                                        teamtoid_goal = one["TeamToId"]
+                                        print('ooo', event_type, playerfromid_goal, teamtoid_goal)
+                                    # Substitution:换人
+                                    if event_type == 'Substitution':
+                                        # 被换的球员
+                                        playerfromid = one["PlayerFromId"]
+                                        # 换上场的球员
+                                        playertoid = one["PlayerToId"]
+                                        # 换人的球队
+                                        teamfromid = one["TeamFromId"]
+                                        print('kkk', event_type, playerfromid, playertoid, teamfromid)
     # ==========================================================
     # event_box：后一位是分钟数
     # ['Goal', 28], ['Assist', 29], ['Tackle', 29]
     # ==========================================================
+    c = Counter(total_type)  # 各event发生的次数
+    print(c)
+    # ==========================================================
+    # event日志有重复，需去重
+    # Counter({'ThrowIn': 46, 'Dribbling': 30, 'FreeKick': 25, 'Foul': 23, 'Tackle': 19, 'Shot': 17, 'AerialDuel': 14,
+    #         'Save': 8, 'Corner': 8, 'Claim': 7, 'Goal': 6, 'Substitution': 5, 'BigChance': 4, 'DroppedBall': 4,
+    #         'YellowCard': 3, 'Assist': 3, 'Offside': 2, 'StartTime': 2, 'VarNotification': 2, 'EndTime': 2,
+    #         'TossCoin': 1, 'Punch': 1, 'EndMatch': 1})
+    # ==========================================================
 
 
+# 根据球队或球员id查找名字，查找getLineups，但似乎换人没有在名单里
+def search_id(id):
+    for dir_item in os.listdir(path):
+        if dir_item.endswith("getLineups.json"):
+            dir_item_path = os.path.join(path, dir_item)
+            if os.path.isfile(dir_item_path):
+                with open(dir_item_path, 'r') as f:
+                    match_info = json.load(f)
+                    #  主队和客队
+                    two_teams = ["HomeLineUpTeamDto", "AwayLineUpTeamDto"]
+                    for team in two_teams:
+                        single_team = match_info["MatchLineups"][team]
+                        team_id = single_team["ID"]
+                        print(team_id, team)
+                        if str(id) == str(team_id):
+                            team_name = single_team["Name"]
+                            team_name_ch = traslation(team_name)
+                            print(team_name_ch)
+                        single_team_info = match_info['MatchLineups'][team]['Pitch']
+                        # country = match_info['MatchLineups'][team]['OfficialName']
+                        # country_name = traslation(country)
+                        for player in single_team_info:
+                            player_id = player["ID"]
+                            if id == player_id:
+                                player_name = team["CommonName"]
+                                player_name_ch = traslation(player_name)
+                                print(player_name_ch)
 
-    # for i in event_box:
-    #     print(i)
-    #                 # for team in two_teams:
-    #                 #     mb_box = list()
-    #                 #     single_team = match_info['MatchLineups'][team]['Pitch']
-    #                 #     country = match_info['MatchLineups'][team]['OfficialName']
-    #                 #     country_name = traslation(country)
-    #                 #     for player in single_team:
-    #                 #         # print(player['CommonName'],player['NumOrder'])
-    #                 #         member = "{0}-{1}".format(traslation(player['CommonName']), player['NumOrder'])
-    #                 #         mb_box.append(member)
-    #                 #     line_up = "、".join(mb_box)
-    #                 #     para_line_up = '首发及换人\n\n{0}首发：{1}\n'.format(country_name, line_up)
-    #                 #     print(para_line_up)
-    # return para_line_up
 
 if __name__ == '__main__':
     # generate_para1()
